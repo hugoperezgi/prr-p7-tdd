5cd99872646602183f52e319bf7ad8be2e999edd
hugop <h.perez10@usp.ceu.es>
2023-05-18 00:57:14 +0200

    fuck

--- srv.py
+++ srv.py
@@ -5,6 +5,7 @@ def setUpSock(ip: str='127.0.0.1', port: int=6969, mode: int=1):
     '''mode 1 = tcp; mode 0 = udp'''
     if mode: s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     else: s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((ip,port))
     return s
 
