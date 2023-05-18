54814db87044952c9f706ecfd1387f65cb71748d
hugop <h.perez10@usp.ceu.es>
2023-05-18 17:46:33 +0200

    test logIn

--- test/cli_test.py
+++ test/cli_test.py
@@ -1,11 +1,18 @@
 from cli import *
-import unittest, socket
+import unittest, socket, hashlib
 
 class TestClient(unittest.TestCase):
 
     def test_setUpSock(self):
         self.assertIsInstance(setUpSock()[0],socket.socket)
         self.assertIsInstance(setUpSock()[1],socket.socket)
+
+    def test_logIn(self):
+        hashgen = hashlib.sha512()
+        usr=b'fuck'
+        psw=b'you'
+        hashgen.update(psw)
+        self.assertEqual(logIn(usr,psw),(usr,hashgen.digest()))
 
 if __name__ == "__main__": 
     unittest.main()
